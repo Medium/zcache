21736d8a360a8b49e9e8a6dc412bcae9019d23e6
Jeremy Stanley <jeremy@obvious.com>
2013-01-02 00:17:29 -0800

    testing CacheManager flow

--- test/manager.js
+++ test/manager.js
@@ -1,5 +1,6 @@
 var CacheCluster = require('../lib/CacheCluster')
 var CacheManager = require('../lib/CacheManager')
+var Q = require('kew')
 
 exports.testManager = function (test) {
   var cacheManager = new CacheManager()
@@ -9,18 +10,64 @@ exports.testManager = function (test) {
   primaryCluster.setServerCapacity('localhost:11213', 5)
   cacheManager.addCluster('primary', primaryCluster, 1)
 
-  primaryCluster.getClientsForKeys(['a', 'b', 'c', 'd'])
-
-  setTimeout(function () {
-    primaryCluster.getClientsForKeys(['a', 'b', 'c', 'd'])
-  }, 5000)
-
-  /*
   var secondaryCluster = new CacheCluster()
   secondaryCluster.setServerCapacity('localhost:11214', 100)
   secondaryCluster.setServerCapacity('localhost:11215', 100)
   cacheManager.addCluster('secondary', secondaryCluster, 2)
-  */
+
+  setTimeout(function () {
+    cacheManager.set("a", "123")
+      .then(function (data) {
+        console.log("SET", data)
+        primaryCluster.setServerCapacity('localhost:11212', 0)
+        primaryCluster.setServerCapacity('localhost:11213', 0)
+        return cacheManager.get("a")
+      })
+      .then(function (data) {
+        console.log("GET", data)
+        return cacheManager.set("a", "456")
+      })
+      .then(function (data) {
+        console.log("SET", data)
+        return cacheManager.get("a")
+      })
+      .then(function (data) {
+        console.log("GET", data)
+        return cacheManager.del("a")
+      })
+      .then(function (data) {
+        console.log("DEL", data)
+        return cacheManager.get("a")
+      })
+      .then(function (data) {
+        console.log("GET", data)
+        primaryCluster.setServerCapacity('localhost:11212', 5)
+        primaryCluster.setServerCapacity('localhost:11213', 5)
+        var defer = Q.defer()
+        setTimeout(function () {
+          defer.resolve(cacheManager.get("a"))
+        }, 1000)
+        return defer.promise
+      })
+      .then(function (data) {
+        console.log("GET", data)
+        return cacheManager.del("a")
+      })
+      .then(function (data) {
+        console.log("DEL", data)
+        return cacheManager.get("a")
+      })
+      .then(function (data) {
+        console.log("GET", data)
+        primaryCluster.setServerCapacity('localhost:11212', 5)
+        primaryCluster.setServerCapacity('localhost:11213', 5)
+      })
+      .fail(function (e) {
+        console.error("ERROR", e, e.stack)
+        throw e
+      })
+      .end()
+  }, 2000)
 
   setTimeout(function () {
     test.done()

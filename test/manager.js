572e208413adb9b59b45ecb2b60cd6b7f283c93d
Jeremy Stanley <jeremy@obvious.com>
2013-01-08 08:25:14 -0800

    Added FakeMemcache

--- test/manager.js
+++ test/manager.js
@@ -1,5 +1,6 @@
 var CacheCluster = require('../lib/CacheCluster')
 var CacheManager = require('../lib/CacheManager')
+var FakeMemcache = require('../lib/FakeMemcache')
 var Q = require('kew')
 
 process.on('uncaughtException', function (e) {
@@ -8,68 +9,71 @@ process.on('uncaughtException', function (e) {
 exports.testManager = function (test) {
   var cacheManager = new CacheManager()
 
-  var primaryCluster = new CacheCluster()
+  var primaryCluster = new CacheCluster({
+    clientCtor: FakeMemcache
+  })
   primaryCluster.setServerCapacity('localhost:11212', 5)
   primaryCluster.setServerCapacity('localhost:11213', 5)
   cacheManager.addCluster('primary', primaryCluster, 1)
 
-  var secondaryCluster = new CacheCluster()
+  var secondaryCluster = new CacheCluster({
+    clientCtor: FakeMemcache
+  })
   secondaryCluster.setServerCapacity('localhost:11214', 100)
   secondaryCluster.setServerCapacity('localhost:11215', 100)
   cacheManager.addCluster('secondary', secondaryCluster, 2)
 
+  var defer = Q.defer()
   setTimeout(function () {
-    cacheManager.set("a", "123")
-      .then(function (data) {
-        console.log("SET", data)
-        primaryCluster.setServerCapacity('localhost:11212', 0)
-        primaryCluster.setServerCapacity('localhost:11213', 0)
-        return cacheManager.get("a")
-      })
-      .then(function (data) {
-        console.log("GET", data)
-        return cacheManager.set("a", "456")
-      })
-      .then(function (data) {
-        console.log("SET", data)
-        return cacheManager.get("a")
-      })
-      .then(function (data) {
-        console.log("GET", data)
-        return cacheManager.del("a")
-      })
-      .then(function (data) {
-        console.log("DEL", data)
-        return cacheManager.get("a")
-      })
-      .then(function (data) {
-        console.log("GET", data)
-        primaryCluster.setServerCapacity('localhost:11212', 5)
-        primaryCluster.setServerCapacity('localhost:11213', 5)
-        var defer = Q.defer()
-        setTimeout(function () {
-          defer.resolve(cacheManager.get("a"))
-        }, 1000)
-        return defer.promise
-      })
-      .then(function (data) {
-        console.log("GET", data)
-        return cacheManager.del("a")
-      })
-      .then(function (data) {
-        console.log("DEL", data)
-        return cacheManager.get("a")
-      })
-      .then(function (data) {
-        console.log("GET", data)
-      })
-      .fail(function (e) {
-        console.error("ERROR", e, e.stack)
-        throw e
-      })
-  }, 2000)
+    defer.resolve(true)
+  }, 200)
 
-  setTimeout(function () {
-    test.done()
-  }, 300000)
+  defer.promise
+    .then(function () {
+      return cacheManager.set("a", "123")
+    })
+    .then(function (data) {
+      test.equal(data, true, "Should set key")
+      primaryCluster.setServerCapacity('localhost:11212', 0)
+      primaryCluster.setServerCapacity('localhost:11213', 0)
+      return cacheManager.get("a")
+    })
+    .then(function (data) {
+      test.equal(data, '123', "Should get key")
+      return cacheManager.set("a", "456")
+    })
+    .then(function (data) {
+      test.equal(data, true, "Should set key again")
+      return cacheManager.get("a")
+    })
+    .then(function (data) {
+      test.equal(data, '456', "Should get key again")
+      return cacheManager.del("a")
+    })
+    .then(function (data) {
+      test.equal(data, true, "Should delete key")
+      return cacheManager.get("a")
+    })
+    .then(function (data) {
+      test.equal(data, undefined, "Should get undefined")
+      primaryCluster.setServerCapacity('localhost:11212', 5)
+      primaryCluster.setServerCapacity('localhost:11213', 5)
+      var defer = Q.defer()
+      setTimeout(function () {
+        defer.resolve(cacheManager.get("a"))
+      }, 1000)
+      return defer.promise
+    })
+    .then(function (data) {
+      test.equal(data, '123', "Should get original key")
+      return cacheManager.del("a")
+    })
+    .then(function (data) {
+      test.equal(data, true, "Should delete key again")
+      return cacheManager.get("a")
+    })
+    .then(function (data) {
+      test.equal(data, undefined, "Should get undefined again")
+      test.done()
+    })
 }

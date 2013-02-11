1ec459422a8e81030c483de4c00900caba3d5249
Jeremy Stanley <jeremy@obvious.com>
2013-02-11 10:16:20 -0800

    Adding base64 support to MemcacheConnection

--- test/test_MemcacheConnection.js
+++ test/test_MemcacheConnection.js
@@ -59,3 +59,62 @@ exports.testMemcacheConnection = function (test) {
 
   cacheInstance.connect()
 }
+
+exports.testMemcacheConnectionBase64 = function (test) {
+  var cacheInstance = new zcache.MemcacheConnection("localhost", 11212, 'base64')
+
+  test.equal(cacheInstance.isAvailable(), false, "Connection should not be available")
+
+  cacheInstance.on('connect', function () {
+    cacheInstance.removeAllListeners('connect')
+    test.equal(cacheInstance.isAvailable(), true, "Connection should be available")
+
+    cacheInstance.set('abc', '123', 300000)
+      .then(function () {
+        cacheInstance.disconnect()
+
+        // wait to ensure reconnection
+        var defer = Q.defer()
+        setTimeout(function () {
+          defer.resolve(true)
+        }, 200)
+        return defer.promise
+      })
+      .then(function () {
+        cacheInstance.connect()
+
+        // wait to ensure reconnection
+        var defer = Q.defer()
+        setTimeout(function () {
+          defer.resolve(true)
+        }, 200)
+        return defer.promise
+      })
+      .then(function () {
+        return cacheInstance.mget(['abc'])
+      })
+      .then(function (vals) {
+        test.equal(vals[0], '123')
+        return cacheInstance.del('abc')
+      })
+      .then(function () {
+        return cacheInstance.mget(['abc'])
+      })
+      .then(function (vals) {
+        test.equal(vals[0], undefined)
+        cacheInstance.destroy()
+      })
+      .fail(function (e) {
+        console.error(e)
+        test.fail(e.message)
+        test.done()
+      })
+  })
+
+  cacheInstance.on('destroy', function () {
+    test.equal(cacheInstance.isAvailable(), false, "Connection should not be available")
+    test.done()
+  })
+
+  cacheInstance.connect()
+}

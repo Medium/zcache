ed74515f95cf75a6a29e0f2b924dd96058494f87
Xiao Ma <x@medium.com>
2014-01-14 15:26:00 -0800

    Handle partial failure from mget()/mset()

--- test/test_CacheCluster.js
+++ test/test_CacheCluster.js
@@ -9,6 +9,7 @@ var Q = require('kew')
 var nodeunitq = require('nodeunitq')
 var builder = new nodeunitq.Builder(exports)
 var logger = require('logg').getLogger('test CacheCluster')
+var PartialResultError = require('../lib/PartialResultError')
 
 builder.add(function testSetAndGet(test) {
   var cluster = new zcache.CacheCluster()
@@ -61,9 +62,86 @@ builder.add(function testMgetAndMget(test) {
       return cluster.mget(keys)
     })
     .then(function(data) {
+      test.equals(100, data.length, 'expect: # of returned value === # of keys')
       for (var i = 0; i < 100; i++) {
         test.equals('value' + i, data[i])
       }
+    })
+})
+
+builder.add(function testPartialMgetFailure(test) {
+  var cluster = new zcache.CacheCluster()
+  var fakeCache1 = new zcache.FakeCache(logger)
+  cluster.addNode('FakeCache1', fakeCache1, 1, 0)
+  cluster.addNode('FakeCache2', new zcache.FakeCache(logger), 1, 0)
+  cluster.addNode('FakeCache3', new zcache.FakeCache(logger), 1, 0)
+  cluster.connect()
+
+  var items = []
+  for (var i = 0; i < 100; i++) {
+    items.push({
+      key: 'key' + i,
+      value: 'value' + i
+    })
+  }
+
+  return cluster.mset(items)
+    .then(function() {
+      var keys = []
+      for (var j = 0; j < 100; j++) {
+        keys.push('key' + j)
+      }
+      // the first cache instance will fail for the next request
+      fakeCache1.setFailureCount(1)
+      return cluster.mget(keys)
+    })
+    .then(function () {
+      test.fail('The mget() call is supposed to fail')
+    })
+    .fail(function (err) {
+      test.ok(err instanceof PartialResultError)
+      var data = err.getData()
+      var error = err.getError()
+      for (var i = 0; i < 100; i++) {
+        var key = 'key' + i
+        if (key in data) {
+          test.equals('value' + i, data[key])
+        } else {
+          test.ok(key in error)
+        }
+      }
+      test.ok(Object.keys(data).length > 60 && Object.keys(data).length < 100, 'Expect partial failures')
+    })
+})
+
+builder.add(function testPartialMsetFailure(test) {
+  var cluster = new zcache.CacheCluster()
+  var fakeCache1 = new zcache.FakeCache(logger)
+  cluster.addNode('FakeCache1', fakeCache1, 1, 0)
+  cluster.addNode('FakeCache2', new zcache.FakeCache(logger), 1, 0)
+  cluster.addNode('FakeCache3', new zcache.FakeCache(logger), 1, 0)
+  cluster.connect()
+
+  var items = []
+  for (var i = 0; i < 100; i++) {
+    items.push({
+      key: 'key' + i,
+      value: 'value' + i
+    })
+  }
+
+  // the first cache instance will fail for the next request
+  fakeCache1.setFailureCount(1)
+  return cluster.mset(items)
+    .then(function () {
+      test.fail('The mset() call is supposed to fail')
+    })
+    .fail(function (err) {
+      test.ok(err instanceof PartialResultError)
+      var data = err.getData()
+      var error = err.getError()
+      test.deepEqual({}, data)
+      test.ok(Object.keys(error).length > 0 && Object.keys(error).length < 40, 'Expect partial failures')
     })
 })
 
@@ -110,7 +188,7 @@ builder.add(function testDel(test) {
     })
 })
 
-// We trust the hashring library for key distribution. This is more
+// We trust the HashRing library for key distribution. This is more
 // of a sanity check to make sure we didn't break things. We are not
 // actually to test how evenly the keys are distributed.
 // This test case should be updated when we change the hashring
@@ -141,5 +219,4 @@ builder.add(function testKeyDistribution(test) {
       test.equals(2050, Object.keys(cluster._servers['FakeCache4'].getData()).length)
       test.equals(1776, Object.keys(cluster._servers['FakeCache5'].getData()).length)
     })
-
 })

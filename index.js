543744b1845a7a0ad60c9dd5844762c729c20367
Xiao Ma <x@medium.com>
2014-04-06 00:04:47 -0700

    Remove unused code that has issues

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 module.exports = {
   CacheCluster: require('./lib/CacheCluster'),
   CacheInstance: require('./lib/CacheInstance'),
-  ConnectionPool: require('./lib/ConnectionPool'),
   ConnectionWrapper: require('./lib/ConnectionWrapper'),
   MemcacheConnection: require('./lib/MemcacheConnection'),
   MultiplexingCache: require('./lib/MultiplexingCache'),
